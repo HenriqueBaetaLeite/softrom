21042927f64e4ea76848399930ae17ea0f053cde
henrique <hbaetaleite@gmail.com>
2021-02-19 12:31:55 -0300

    corrigindo campo obs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,7 @@ import {
   Box,
   Card,
   Button,
+  Typography,
 } from '@material-ui/core';
 
 import { getAllCompanys } from '../service/companys/companys';
@@ -35,10 +36,10 @@ const TableCompanys = () => {
 
   return (
     <Container>
-      <Box display="flex" justifyContent="space-around" alignItems="center">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
         <Box>
-          <h2>Boas vindas ao App Soft-Rom Sistemas</h2>
-          <h5>22 anos de experiência no mercado</h5>
+          <Typography variant="h4">Boas vindas ao App Soft-Rom Sistemas</Typography>
+          <Typography variant="h6">22 anos de experiência no mercado</Typography>
         </Box>
         <img
           height="40px"
@@ -47,7 +48,7 @@ const TableCompanys = () => {
         />
       </Box>
       <Card>
-        <h2>Tabela de empresas</h2>
+        <Typography variant="h5">Tabela de empresas</Typography>
 
         {companys.length === 0 ? (
           <CircularProgress />
@@ -72,8 +73,9 @@ const TableCompanys = () => {
                   </TableCell>
                   <TableCell align="center">{company.tipo_doc}</TableCell>
                   <TableCell align="center">{company.documento}</TableCell>
-                  <TableCell align="center">{company.gerar_nf ? 'yes' : 'no'}</TableCell>
-                  <TableCell align="center">{company.retem_iss ? 'yes' : 'no'}</TableCell>
+                  <TableCell align="center">{company.gerar_nf ? 'sim' : 'não'}</TableCell>
+                  <TableCell align="center">{company.retem_iss ? 'sim' : 'não'}</TableCell>
+                  <TableCell align="center">{company.obs}</TableCell>
                   <TableCell align="center">
                     <Button
                       variant="outlined"
